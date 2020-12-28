060d94e5a1187b57b72378b8e9db17421ba9d838
M Algah Fattah Illahi <herppratama@gmail.com>
2020-12-28 08:25:43 +0700

    remove db connection

--- auth-service/index.js
+++ auth-service/index.js
@@ -2,22 +2,3 @@ require('app-module-path').addPath(__dirname);
 require('dotenv').config()
 require('app');
 
-const mongoose = require('mongoose');
-
-const cfg = require('app/configs');
-
-mongoose
-  .connect(
-    `${cfg.MONGO_HOST}/${cfg.MONGO_DATABASE}?${cfg.MONGO_OPTIONS}`,
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    }
-  )
-  .then(async () => {
-    await require('app');
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
